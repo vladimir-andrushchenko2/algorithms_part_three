ec8a12315fd660668dbd7571ef8aea18cce5193f
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-03-15 22:21:54 +0300

    OK

--- 7H_flower_field/main.cpp
+++ 7H_flower_field/main.cpp
@@ -46,8 +46,50 @@ Matrix TraverseField(const Field& field) {
     return memo;
 }
 
+std::string GetOptimalRoute(Matrix matrix) {
+    int x = static_cast<int>(matrix.front().size()) - 1;
+
+    int y = static_cast<int>(matrix.size()) - 1;
+
+    std::string output;
+    while (x > 0 || y > 0) {
+        if (x == 0) {
+            output.push_back('U');
+
+            --y;
+
+            continue;
+        }
+
+        if (y == 0) {
+            output.push_back('R');
+
+            --x;
+
+            continue;
+        }
+
+        if (matrix[y][x - 1] < matrix[y - 1][x]) {
+            output.push_back('U');
+
+            --y;
+
+        } else {
+            output.push_back('R');
+
+            --x;
+        }
+    }
+
+    return {output.rbegin(), output.rend()};
+}
+
 int main(int argc, const char * argv[]) {
-    std::cout << TraverseField(ReadField(std::cin)).back().back() << '\n';
-    
+    auto matrix = TraverseField(ReadField(std::cin));
+
+    std::cout << matrix.back().back() << '\n';
+
+    std::cout << GetOptimalRoute(std::move(matrix)) << '\n';
+
     return 0;
 }
