84a9c2ae14dcbeedff407bf476411376bd3c5d47
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-18 10:56:27 +0300

    OK

--- gold_chunks/main.cpp
+++ gold_chunks/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[]) {
         chunks.push_back(chunk);
     }
 
-    std::vector<std::vector<int>> dp(gold_chunks, std::vector<int>(capacity));
+    std::vector<std::vector<int>> dp(gold_chunks, std::vector<int>(capacity + 1));
 
     constexpr int first_chunk_id = 0;
 
@@ -27,17 +27,20 @@ int main(int argc, const char * argv[]) {
         }
     }
 
+    constexpr int second_chunk_id = 1;
 
     // fill remaining rows
-    for (int chunk_id = 1; chunk_id < gold_chunks; ++chunk_id) {
-        for (int weight = 0; weight < capacity; ++weight) {
-            int remaining_capacity = weight - chunks[chunk_id];
-
-            if (remaining_capacity > 0) {
-
+    for (int chunk_id = second_chunk_id; chunk_id < gold_chunks; ++chunk_id) {
+        for (int weight = 0; weight <= capacity; ++weight) {
+            if (weight - chunks[chunk_id] >= 0) {
+                dp[chunk_id][weight] = std::max(dp[chunk_id - 1][weight], chunks[chunk_id] + dp[chunk_id - 1][weight - chunks[chunk_id]]);
+            } else {
+                dp[chunk_id][weight] = dp[chunk_id - 1][weight];
             }
         }
     }
+    
+    std::cout << dp.back().back() << '\n';
 
     return 0;
 }
