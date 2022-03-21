f4df9c1ecc50c6d6d6092e49cb267eebd1b64c5d
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-21 20:56:43 +0300

    chore

---
+++ 8K_compare_even_letters/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+bool EvenLetter(char letter) {
+    return (letter - 'a' + 1) % 2 == 0;
+}
+
+int main() {
+    std::string first, second;
+    
+    
+    
+    return 0;
+}
