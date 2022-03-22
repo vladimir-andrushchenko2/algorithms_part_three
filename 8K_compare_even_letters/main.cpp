bd91786cf4c5ec8e08e4f585e0b04c68d85cc1c1
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-03-22 13:22:39 +0300

    chore

--- 8K_compare_even_letters/main.cpp
+++ 8K_compare_even_letters/main.cpp
@@ -6,10 +6,44 @@ bool EvenLetter(char letter) {
     return (letter - 'a' + 1) % 2 == 0;
 }
 
+struct EvenString {
+    EvenString(const std::string& data) : data(data) {
+        while (index < data.size() && !EvenLetter(data[index])) {
+            ++index;
+        }
+
+        if (!EvenLetter(data[index])) {
+            ++index;
+        }
+    }
+
+    char GetNextChar() {
+        while (!EvenLetter(data[index]) && index < data.size()) {
+            ++index;
+        }
+
+        if (index == data.size()) {
+            assert(false);
+        }
+
+        return data[index];
+    }
+
+    bool IsEmpty() const {
+        return index == data.size();
+    }
+
+    int index{};
+
+    const std::string& data;
+};
+
 int main() {
-    std::string first, second;
+    std::string left, right;
     
-    
+    std::cin >> left >> right;
+
+
     
     return 0;
 }
