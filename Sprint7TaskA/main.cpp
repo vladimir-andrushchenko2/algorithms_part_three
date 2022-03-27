494358a717c39d115aa67a86f74b5a56e0250a12
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-27 16:34:17 +0300

    OK

--- Sprint7TaskA/main.cpp
+++ Sprint7TaskA/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <utility>
 
 /*
 -- ПРИНЦИП РАБОТЫ --
@@ -15,35 +17,28 @@
 */
 
 int LevenshteinDistance(const std::string& source, const std::string& target) {
-    std::vector<std::vector<int>> dp(source.size() + 1, std::vector<int>(target.size() + 1));
+    std::vector<int> previous_column(target.size() + 1);
+    std::vector<int> current_column(target.size() + 1);
     
-    // source of n char can be tranformed to empty target by deleting n elementns
-    static constexpr int kTargetZeroSize = 0;
+    std::iota(previous_column.begin(), previous_column.end(), 0);
+    
+    for (int i = 0; i < source.size(); ++i) {
+        current_column.front() = i + 1;
         
-    for (int source_prefix_size = 1; source_prefix_size <= source.size(); ++source_prefix_size) {
-        dp[source_prefix_size][kTargetZeroSize] = source_prefix_size;
-    }
-    
-    // empty source can be transformed into target by inserting n elements
-    static constexpr int kSourceZeroSize = 0;
-    
-    for (int target_prefix_size = 1; target_prefix_size <= target.size(); ++target_prefix_size) {
-        dp[kSourceZeroSize][target_prefix_size] = target_prefix_size;
-    }
-    
-    for (int source_prefix_size = 1; source_prefix_size <= source.size(); ++source_prefix_size) {
-        for (int target_prefix_size = 1; target_prefix_size <= target.size(); ++target_prefix_size) {
-            int substitution_cost = source.at(source_prefix_size - 1) == target.at(target_prefix_size - 1) ? 0 : 1;
+        for (int j = 0; j < target.size(); ++j) {
+            int deletion_cost = previous_column[j + 1] + 1;
             
-            dp[source_prefix_size][target_prefix_size] = std::min({
-                dp[source_prefix_size - 1][target_prefix_size] + 1,
-                dp[source_prefix_size][target_prefix_size - 1] + 1,
-                dp[source_prefix_size - 1][target_prefix_size - 1] + substitution_cost
-            });
+            int insertion_cost = current_column[j] + 1;
+            
+            int substitution_cost = source[i] == target[j] ? previous_column[j] : previous_column[j] + 1;
+            
+            current_column[j + 1] = std::min({deletion_cost, insertion_cost, substitution_cost});
         }
+        
+        std::swap(current_column, previous_column);
     }
     
-    return dp.back().back();
+    return previous_column.back();
 }
 
 int main(int argc, const char * argv[]) {
