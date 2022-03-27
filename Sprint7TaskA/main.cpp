921287494bb1884e138911a73c61bf3eb6b35807
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-03-27 11:04:23 +0300

    Added explanation

--- Sprint7TaskA/main.cpp
+++ Sprint7TaskA/main.cpp
@@ -3,25 +3,42 @@
 #include <vector>
 #include <algorithm>
 
+/*
+-- ПРИНЦИП РАБОТЫ --
+Источником вдохновения послужил https://en.wikipedia.org/wiki/Wagner-Fischer_algorithm в котором мы создаем матрицу для хранения размеров префиксов и вычисляем растояние левенштайна между ними
+ 
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+ O(N * M) где N это количество элементов в первой строке, а M это размер второй строки. Нам необходимо заполнить эту таблицу.
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+ Для решения алгоритма нужно хранить таблицу мемоизации O(N * M).
+*/
+
 int LevenshteinDistance(const std::string& source, const std::string& target) {
     std::vector<std::vector<int>> dp(source.size() + 1, std::vector<int>(target.size() + 1));
     
-    for (int i = 1; i <= source.size(); ++i) {
-        dp[i][0] = i;
+    // source of n char can be tranformed to empty target by deleting n elementns
+    static constexpr int kTargetZeroSize = 0;
+        
+    for (int source_prefix_size = 1; source_prefix_size <= source.size(); ++source_prefix_size) {
+        dp[source_prefix_size][kTargetZeroSize] = source_prefix_size;
     }
     
-    for (int j = 1; j <= target.size(); ++j) {
-        dp[0][j] = j;
+    // empty source can be transformed into target by inserting n elements
+    static constexpr int kSourceZeroSize = 0;
+    
+    for (int target_prefix_size = 1; target_prefix_size <= target.size(); ++target_prefix_size) {
+        dp[kSourceZeroSize][target_prefix_size] = target_prefix_size;
     }
     
-    for (int i = 1; i <= source.size(); ++i) {
-        for (int j = 1; j <= target.size(); ++j) {
-            int substitution_cost = source.at(i - 1) == target.at(j - 1) ? 0 : 1;
+    for (int source_prefix_size = 1; source_prefix_size <= source.size(); ++source_prefix_size) {
+        for (int target_prefix_size = 1; target_prefix_size <= target.size(); ++target_prefix_size) {
+            int substitution_cost = source.at(source_prefix_size - 1) == target.at(target_prefix_size - 1) ? 0 : 1;
             
-            dp[i][j] = std::min({
-                dp[i - 1][j] + 1,
-                dp[i][j - 1] + 1,
-                dp[i - 1][j - 1] + substitution_cost
+            dp[source_prefix_size][target_prefix_size] = std::min({
+                dp[source_prefix_size - 1][target_prefix_size] + 1,
+                dp[source_prefix_size][target_prefix_size - 1] + 1,
+                dp[source_prefix_size - 1][target_prefix_size - 1] + substitution_cost
             });
         }
     }
