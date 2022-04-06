ab98185c721cb9509b5bfac1ed38c377c121575a
Vladimir Andrushchenko <andrushchenko.v@icloud.com>
2022-04-06 21:22:45 +0300

    test tomorrow

--- Sprint8TaskA/main.cpp
+++ Sprint8TaskA/main.cpp
@@ -4,6 +4,8 @@
 #include <string_view>
 #include <stack>
 
+using namespace std::string_literals;
+
 static constexpr int kBasisForHashing = 1283;
 
 static constexpr int kModuloForHashing = 9001;
@@ -29,55 +31,63 @@ int ReadNumber(std::string_view& text) {
     return output;
 }
 
-struct BracketStack {
-//    std::stack<char> brackets;
-    int opening_bracket_counter{};
-    
-    int Size() const {
-        return static_cast<int>(brackets.size());
-    }
-    
-    bool Empty() const {
-        return Size() == 0;
-    }
-    
-    void AddClosingBracket() {
-        if (brackets.empty()) {
-            assert(false);
-        }
-        
-        brackets.pop();
-    }
-    
-    void AddOpeningBracket() {
-        brackets.push('[');
-    }
-};
-
 std::string_view GetCompressedPartInBrackets(std::string_view text) {
     assert(text.front() == '[');
     
-    BracketStack stack;
+    int opened_bracket_counter = 1;
     
-    text.remove_prefix(1);
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == '[') {
+            ++opened_bracket_counter;
+        }
+        
+        if (text[i] == ']') {
+            --opened_bracket_counter;
+            
+            if (opened_bracket_counter == 0) {
+                return text.substr(1, i);
+            }
+        }
+    }
     
-    stack.AddOpeningBracket();
+    assert(false);
+    return {};
+}
+
+std::string Multiply(std::string text, std::size_t times) {
+    std::string_view source = text;
     
+    for (int i = 0; i < times; ++i) {
+        text.append(source);
+    }
     
+    return text;
 }
 
 std::string Unpack(std::string_view text) {
-    std::string output;
+    std::string prefix;
     
     while (!text.empty()) {
         if (std::isdigit(text.front())) {
+            int n = ReadNumber(text);
             
+            std::string_view compressed_part = GetCompressedPartInBrackets(text);
+            
+            text.remove_prefix(compressed_part.size());
+            
+            return prefix + Multiply(Unpack(compressed_part), n) + Unpack(text);
         }
+        
+        prefix.push_back(text.front());
+        text.remove_prefix(1);
     }
+    
+    return prefix;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string unpacked = Unpack("2[a]2[ab]"s);
+    
+    assert(unpacked == "aaabab"s);
     return 0;
 }
