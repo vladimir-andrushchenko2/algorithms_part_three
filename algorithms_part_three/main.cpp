fb6e41a68cfc10fe2994bd57de9d2facf4ff07d5
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-04-15 22:17:36 +0300

    passed C. Самый длинный палиндром 2

--- algorithms_part_three/main.cpp
+++ algorithms_part_three/main.cpp
@@ -1,14 +1,61 @@
-//
-//  main.cpp
-//  algorithms_part_three
-//
-//  Created by Vladimir Andrushchenko on 13.03.2022.
-//
-
 #include <iostream>
+#include <map>
+#include <string>
+#include <string_view>
+
+using Dict = std::map<char, int>;
+
+Dict CountLetters(std::string_view text) {
+    Dict output;
+
+    for (char symbol : text) {
+        ++output[symbol];
+    }
+
+    return output;
+}
+
+std::string GetLongesAnagram(const Dict& dict) {
+    std::string output;
+
+    bool is_middle_letter_found = false;
+
+    char middle_letter = '\0';
+
+    for (auto [symbol, count] : dict) {
+        if (!is_middle_letter_found) {
+            // if odd
+            if (count % 2 == 1) {
+                is_middle_letter_found = true;
+
+                middle_letter = symbol;
+            }
+        }
+
+        int pairs = count / 2;
+
+        for (int i = 0; i < pairs; ++i) {
+            output.push_back(symbol);
+        }
+    }
+
+    int first_half_size = static_cast<int>(output.size());
+
+    if (is_middle_letter_found) {
+        output.push_back(middle_letter);
+    }
+
+    for (int i = first_half_size - 1; i >= 0; --i) {
+        output.push_back(output[i]);
+    }
+
+    return output;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string text;
+
+    std::getline(std::cin, text);
+    std::cout << GetLongesAnagram(CountLetters(text)) << '\n';
     return 0;
 }
